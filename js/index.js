52e304166e821d8ace1fa7652b66cdc25b3eb1d8
ALNoman78 <abdullaalno078@gmail.com>
2024-09-21 10:39:03 +0600

    ticket price will be global veriable

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // this is seat number 2 , click event handler will be processed
+let ticketPrice = 550;
 
 document.getElementById("text-seat-b2").addEventListener("click", function e() {
   console.log('B2 clicked');
@@ -7,9 +8,8 @@ document.getElementById("text-seat-b2").addEventListener("click", function e() {
   document.getElementById('text-seat-b2').style.color = "#fff";
   document.getElementById('text-seat-b2').style.fontWeight = "bold"
 
-    const ticketPrice = 550;
-    const mainElement = (document.getElementById("total-price-bdt").innerText =
-        ticketPrice);
+    // const ticketPrice = 550;
+    const mainElement = document.getElementById("total-price-bdt").innerText = ticketPrice;
     console.log(mainElement);
 
     const ticketP = document.createElement("div");
@@ -31,20 +31,20 @@ document.getElementById("text-seat-b2").addEventListener("click", function e() {
 
 document.getElementById("text-seat-b1").addEventListener("click", function handler() {
 
-  document.getElementById('text-seat-b2').disabled = true;
+  document.getElementById('text-seat-b2').classList.toggle ('selected');
   document.getElementById('text-seat-b1').style.backgroundColor = '#1DD100'
   document.getElementById('text-seat-b1').style.color = '#fff'
   document.getElementById('text-seat-b1').style.fontWeight = 'bold'
-  const ticketPriceB2 = 550;
+  // const ticketPrice = 550;
   const totalPrice = document.getElementById("total-price-bdt");
-  totalPrice.innerText = ticketPriceB2;
+  totalPrice.innerText = ticketPrice;
   // console.log(totalPrice);
 
   const newDiv = document.createElement("div");
   newDiv.innerHTML = `
         <div class = "flex justify-between">
             <p>Economy Class ticket price of B2</p>
-            <p>${ticketPriceB2}</p>
+            <p>${ticketPrice}</p>
         </div>
     `;
   document.getElementById("ticket-select-show").append(newDiv);
@@ -58,7 +58,6 @@ document.getElementById('bus-h-seat').addEventListener('click', function e(){
   document.getElementById('bus-h-seat').style.backgroundColor = '#1DD100';
   document.getElementById('bus-h-seat').style.color = 'white';
   document.getElementById('bus-h-seat').style.fontWeight = 'bold';
-  const ticketPrice = 550;
   const newDiv = document.createElement("div");
   newDiv.innerHTML = `
         <div class = "flex justify-between">
@@ -68,4 +67,5 @@ document.getElementById('bus-h-seat').addEventListener('click', function e(){
     `;
   document.getElementById('ticket-select-show').appendChild(newDiv)
   this.removeEventListener('click', e);
+  document.getElementById('ticket-select-show').style.backgroundColor = "none";
 })
